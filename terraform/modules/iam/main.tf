08d546bb2c42f3bd10187bd941d348bf1f95df3b
Rahul Deokate <rahuldeokate47666@gmail.com>
2025-08-03 08:22:33 +0530

    Added role

---
+++ terraform/modules/iam/main.tf
@@ -0,0 +1,24 @@
+# EKS Cluster Assume Role Policy
+data "aws_iam_policy_document" "eks_cluster_assume" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+# EKS Cluster IAM Role
+resource "aws_iam_role" "eks_cluster_role" {
+  name               = "eks-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume.json
+}
+
+# Attach EKS Cluster Policy
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws_
