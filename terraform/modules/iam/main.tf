6651999bfd63c0a8a6e282cfc27a20dc0effe717
Rahul Deokate <rahuldeokate47666@gmail.com>
2025-08-03 08:45:40 +0530

    Infrastructure code is ready

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -21,4 +21,5 @@ resource "aws_iam_role" "eks_cluster_role" {
 # Attach EKS Cluster Policy
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws_
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
--- terraform/modules/nodegroup/variables.tf
+++ terraform/modules/nodegroup/variables.tf
@@ -1,15 +1,15 @@
 variable "cluster_name" {
-  description = "Name of the cluster"
+  description = "Name of the EKS cluster"
   type        = string
 }
 
 variable "subnet1_id" {
-  description = "ID of the subnet 1"
+  description = "ID of subnet 1"
   type        = string
 }
 
 variable "subnet2_id" {
-  description = "ID of the subnet 2"
+  description = "ID of subnet 2"
   type        = string
 }
 
@@ -19,10 +19,25 @@ variable "node_role_arn" {
 }
 
 variable "desired_capacity" {
-  description = "Desired capacity of the nodes"
+  description = "Desired number of worker nodes"
   type        = number
   default     = 1
 }
 
 variable "min_size" {
-  description = "Minimum capacity
+  description = "Minimum number of worker nodes"
+  type        = number
+  default     = 1
+}
+
+variable "max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 1
+}
+
+variable "instance_types" {
+  description = "List of EC2 instance types to use for nodes"
+  type        = list(string)
+  default     = ["t2.medium"]
+}
