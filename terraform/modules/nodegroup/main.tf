1c301719c22f035cf7ebfccaa5af6b292eb1da34
Rahul Deokate <rahuldeokate47666@gmail.com>
2025-08-03 08:28:12 +0530

    Added node group in cluster

---
+++ terraform/modules/nodegroup/main.tf
@@ -0,0 +1,22 @@
+resource "aws_eks_node_group" "workers" {
+  cluster_name    = var.cluster_name
+  node_group_name = "${var.cluster_name}-nodegroup"
+  node_role_arn   = var.node_role_arn
+
+  subnet_ids = [var.subnet1_id, var.subnet2_id]
+
+  scaling_config {
+    desired_size = var.desired_capacity
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
+
+  instance_types = var.instance_types
+  ami_type       = "AL2_x86_64"
+  disk_size      = 10
+  capacity_type  = "ON_DEMAND"
+
+  depends_on = [
+    aws_eks_cluster.eks  # Depends on cluster creation, not itself
+  ]
+}
--- terraform/modules/nodegroup/variables.tf
+++ terraform/modules/nodegroup/variables.tf
@@ -1,4 +1,28 @@
-output "nodegroup_name" {
-  description = "Node group name"
-  value       = aws_eks_node_group.workers.node_group_name
+variable "cluster_name" {
+  description = "Name of the cluster"
+  type        = string
 }
+
+variable "subnet1_id" {
+  description = "ID of the subnet 1"
+  type        = string
+}
+
+variable "subnet2_id" {
+  description = "ID of the subnet 2"
+  type        = string
+}
+
+variable "node_role_arn" {
+  description = "IAM role ARN for EC2 nodes"
+  type        = string
+}
+
+variable "desired_capacity" {
+  description = "Desired capacity of the nodes"
+  type        = number
+  default     = 1
+}
+
+variable "min_size" {
+  description = "Minimum capacity
